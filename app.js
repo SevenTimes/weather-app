5ed7e4cc7eafd2c3903d34f7da41a392c938e37f
SevenTimes <krab47@yahoo.com>
2021-07-26 16:11:17 +0400

    add basic dom refresh after input

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
 async function getWeatherData(city) {
-  const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2393c3bafdf1164d3efc533db9ceed34&units=metric`);
+  const weatherResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2393c3bafdf1164d3efc533db9ceed34&units=metric`
+  );
   const weatherJson = await weatherResponse.json();
   if (weatherJson.cod === '404') {
-    alert(weatherJson.message)
+    alert(weatherJson.message);
+    return;
   }
-  console.log(weatherJson);
   const weatherData = new getWeatherObject(weatherJson);
-  console.log(weatherData);
+  updateWeatherInfo(weatherData);
 }
 
-function getWeatherObject(data) {
-  this.cityName = data.name;
-  this.country = data.sys.country;
-  this.weatherDescription = data.weather[0].description;
-  this.weatherIcon = data.weather[0].icon;
-  this.temp = data.main.temp;
-  this.feelsLikeTemp = data.main.feels_like;
-  this.humidity = data.main.humidity;
-  this.windSpeed = data.wind.speed;
-  this.windDirection = degToCompass(data.wind.deg);
+function getWeatherObject(json) {
+  this.cityName = json.name;
+  this.country = json.sys.country;
+  this.weatherDescription = json.weather[0].description;
+  this.weatherIcon = json.weather[0].icon;
+  this.temp = json.main.temp;
+  this.feelsLikeTemp = json.main.feels_like;
+  this.humidity = json.main.humidity;
+  this.windSpeed = json.wind.speed;
+  this.windDirection = degToCompass(json.wind.deg);
 }
 
 function degToCompass(num) {
-  const val = Math.floor((num / 45) + 0.5);
-  const directions = ["N", "NE",  "E",  "SE",  "S",  "SW",  "W",  "NW"];
-  return directions[(val % 8)];
+  const val = Math.floor(num / 45 + 0.5);
+  const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+  return directions[val % 8];
 }
 
-getWeatherData('london');
+function updateWeatherInfo(data) {
+  const location = document.querySelector('#location');
+  location.innerText = `${data.cityName}, ${data.country}`;
+
+  const weatherIcon = document.querySelector('img');
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data.weatherIcon}@4x.png`;
+
+  const temp = document.querySelector('h1');
+  temp.innerText = `${data.temp}°`;
+
+  const feelLike = document.querySelector('p');
+  feelLike.innerText = `Feels like ${data.feelsLikeTemp}°`;
+}
+
+const searchBtn = document.querySelector('#search');
+searchBtn.addEventListener('click', searchWeather);
+
+function searchWeather() {
+  const cityInput = document.querySelector('input').value;
+  getWeatherData(cityInput);
+}
